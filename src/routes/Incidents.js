c2970805086c08c90c542f8dad19666198ca807d
Santiago <pinto.santiago.augusto@gmail.com>
2020-10-07 12:50:19 -0300

    Added http request logic for the /incidents endpoint

--- src/routes/Incidents.js
+++ src/routes/Incidents.js
@@ -1,6 +1,11 @@
 import React from "react";
+import axios from 'axios'
+
 
 function Incidents() {
+    const url = 'https://cors-anywhere.herokuapp.com/https://contame.herokuapp.com/contame/report';
+    axios.get(url).then((response) => {console.log(response.data);});
+    
     return (
         <h1>Aca van los incidentes</h1>
     )
