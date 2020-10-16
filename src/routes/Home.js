71474eaf4aab08a6d3fd56968911356e591a4a32
Lucas Lavandeira <llavandeira@devartis.com>
2020-10-16 19:03:15 -0300

    Add basic home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import {withRouter} from 'react-router-dom';
 
-function Home() {
-  return <h1>Hola mundo!!!</h1>;
-}
-
-export {Home};
+export const Home = withRouter((props) => {
+  return (
+    <Container>
+      <h1>¡Contame! Backoffice</h1>
+      <div>
+        <Button
+          variant="contained"
+          color={'primary'}
+          onClick={() => props.history.push('/incidents/')}>
+          Listado de incidencias
+        </Button>
+      </div>
+    </Container>
+  );
+});
