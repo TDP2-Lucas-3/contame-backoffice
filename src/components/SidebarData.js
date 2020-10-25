d960b41f42288366b65aa14ff1f9b96062a027b1
Santiago <pinto.santiago.augusto@gmail.com>
2020-10-25 14:12:07 -0300

    Removed unused import

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as MdIcons from 'react-icons/md';
 import * as FiIcons from 'react-icons/fi';
