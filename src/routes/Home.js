f6300088b885c62fd487a8b8e08eecb49effd706
Santiago <pinto.santiago.augusto@gmail.com>
2020-10-25 21:59:39 -0300

    Removed unused import and style

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import {withRouter} from 'react-router-dom';
 
@@ -7,14 +6,6 @@ export const Home = withRouter((props) => {
   return (
     <Container>
       <h1>¡Contame! Backoffice</h1>
-      <div>
-        <Button
-          variant="contained"
-          color={'primary'}
-          onClick={() => props.history.push('/incidents/')}>
-          Listado de incidencias
-        </Button>
-      </div>
     </Container>
   );
 });
