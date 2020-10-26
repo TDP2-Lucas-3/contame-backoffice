3ff725ee1a7706252ff035953bebed683048a6ce
Santiago <pinto.santiago.augusto@gmail.com>
2020-10-26 11:04:14 -0300

    Added margin to homepage to fit with the sidebar

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import {withRouter} from 'react-router-dom';
+import './home/Home.css';
 
 export const Home = withRouter((props) => {
   return (
     <Container>
-      <h1>¡Contame! Backoffice</h1>
+      <h1 className="welcomeMessage">¡Contame! Backoffice</h1>
     </Container>
   );
 });
