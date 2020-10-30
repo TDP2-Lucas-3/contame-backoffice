d4a93d220e69be4c139925d33b3c2b64fd5512fe
Lucas Lavandeira <llavandeira@devartis.com>
2020-10-30 18:12:58 -0300

    fix eslint

--- src/incident_details/map.js
+++ src/incident_details/map.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {GoogleMap, Marker, withGoogleMap} from 'react-google-maps';
 
-export const MyMapComponent = withGoogleMap((props) => (
+export const MyMapComponent = withGoogleMap(() => (
   <GoogleMap defaultZoom={8} defaultCenter={{lat: -34.397, lng: 150.644}}>
     {<Marker position={{lat: -34.397, lng: 150.644}} />}
   </GoogleMap>
