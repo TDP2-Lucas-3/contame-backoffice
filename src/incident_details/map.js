8ff0a508942fdc6586bdc909e0636e59722127ef
Lucas Lavandeira <llavandeira@devartis.com>
2020-10-29 21:00:17 -0300

    Add google maps example

---
+++ src/incident_details/map.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {GoogleMap, Marker, withGoogleMap} from 'react-google-maps';
+
+export const MyMapComponent = withGoogleMap((props) => (
+  <GoogleMap defaultZoom={8} defaultCenter={{lat: -34.397, lng: 150.644}}>
+    {<Marker position={{lat: -34.397, lng: 150.644}} />}
+  </GoogleMap>
+));
