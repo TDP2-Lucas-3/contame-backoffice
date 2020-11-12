b58a3602f5a57ac0a64c3a20bffef70b121ecd7e
Santiago <pinto.santiago.augusto@gmail.com>
2020-11-12 12:56:07 -0300

    Added error label that gets displayed on user entering a wrong id or password

--- src/routes/login/LoginContainer.js
+++ src/routes/login/LoginContainer.js
@@ -4,28 +4,53 @@ import {signIn} from '../../services/authentication';
 import {login} from '../../redux/auth';
 import {LoginForm} from './loginForm/LoginForm';
 import {useHistory} from 'react-router-dom';
+import {LoginErrorLabel} from './loginForm/loginErrorLabel/LoginErrorLabel';
 
 export const LoginContainer = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [auth, setAuth] = useState('');
   const dispatch = useDispatch();
   const history = useHistory();
 
   const onSubmit = async () => {
-    const response = await signIn(email, password);
+    let response;
+
+    try {
+      response = await signIn(email, password);
+    } catch {
+      setAuth(false);
+      return;
+    }
     if (response.status === 200) {
+      setAuth(true);
       dispatch(login(response.data.token));
       history.push('/');
     }
   };
 
   return (
-    <LoginForm
-      email={email}
-      setEmail={setEmail}
-      password={password}
-      setPassword={setPassword}
-      onSubmit={onSubmit}
-    />
+    <div>
+      {auth === false ? (
+        <div>
+          <LoginForm
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            onSubmit={onSubmit}
+          />
+          <LoginErrorLabel className="error-labels" />
+        </div>
+      ) : (
+        <LoginForm
+          email={email}
+          setEmail={setEmail}
+          password={password}
+          setPassword={setPassword}
+          onSubmit={onSubmit}
+        />
+      )}
+    </div>
   );
 };
---
+++ src/routes/login/loginForm/loginErrorLabel/LoginErrorLabel.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Styles.css'
+
+export const LoginErrorLabel = () => {
+  return <label className="error-label"> *Usuario o contraseña incorrectos</label>;
+};
