0ad9d2d2e396e0ee4a10c6533377b86df5dcf56a
Santiago <pinto.santiago.augusto@gmail.com>
2020-11-12 13:04:06 -0300

    Fixed code style

--- src/routes/login/loginForm/loginErrorLabel/LoginErrorLabel.js
+++ src/routes/login/loginForm/loginErrorLabel/LoginErrorLabel.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import './Styles.css'
+import './Styles.css';
 
 export const LoginErrorLabel = () => {
-  return <label className="error-label"> *Usuario o contraseña incorrectos</label>;
+  return (
+    <label className="error-label"> *Usuario o contraseña incorrectos</label>
+  );
 };
