fe903bbc5e46d2c3a502b2c31f1b0ad679077414
Santiago <pinto.santiago.augusto@gmail.com>
2020-11-26 21:50:45 -0300

    Added admin default picture for comments

--- src/routes/incidents/detail/comment/profilePicture/ProfilePicture.js
+++ src/routes/incidents/detail/comment/profilePicture/ProfilePicture.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Styles.css';
 import {Container} from 'react-bootstrap';
+const DEFAULT_IMG = process.env.REACT_APP_ADMIN_DEFAULT_PICTURE_URL;
 
 export const ProfilePicture = (props) => {
   return (
     <Container className="profile-picture-container">
-      <img src={props.image} alt={'Profile'} className="profile-picture" />
+      <img
+        src={props.image || DEFAULT_IMG}
+        alt={'Profile'}
+        className="profile-picture"
+      />
     </Container>
   );
 };
