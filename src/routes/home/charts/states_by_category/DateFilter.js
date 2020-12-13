dc6152ac1381a52de5e223d9af50b69f9b34c3ba
Lucas Lavandeira <llavandeira@devartis.com>
2020-12-13 20:53:24 -0300

    Add resets to date pickers

--- src/routes/home/charts/states_by_category/DateFilter.js
+++ src/routes/home/charts/states_by_category/DateFilter.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {DatePicker} from '@material-ui/pickers';
+import {Row} from 'reactstrap';
 
 export const DateFilter = (props) => {
   const [date, setDate] = useState(null);
@@ -8,14 +9,30 @@ export const DateFilter = (props) => {
     setDate(newDate);
     props.onDateChange(newDate.format('YYYY-MM-DD'));
   };
+
+  const clean = () => {
+    if (date === null) {
+      return;
+    }
+
+    setDate(null);
+    props.onDateChange(null);
+  };
+
   return (
-    <DatePicker
-      disableToolbar
-      variant="inline"
-      format="YYYY-MM-DD"
-      label={props.label}
-      value={date}
-      onChange={handleDateChange}
-    />
+    <Row>
+      <DatePicker
+        disableToolbar
+        variant="inline"
+        format="YYYY-MM-DD"
+        autoOk={true}
+        label={props.label}
+        value={date}
+        onChange={handleDateChange}
+      />
+      <p style={{margin: 20}} className={'clean-filters'} onClick={clean}>
+        Limpiar
+      </p>
+    </Row>
   );
 };
